f44ad8c2101e73b4dd598a35e70f7e67149e3080
Tim C <timc@Tims-MacBook-Pro.local>
2026-03-12 22:11:05 -0700

    Update main.py with the nearest neighbor function

--- main.py
+++ main.py
@@ -2,13 +2,38 @@
 import numpy as np
 
 #Load training data
-
 def load_data(filename):
-    return np.loadtxt(filename, delimiter=',')
+    return np.loadtxt(filename)
 
 # Create the euclidean distance function
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
+# Create the nearest neighbor classifier
+def nearest_neighbor(data, feature_set):
+    N = data.shape[0]
+    labels = data[:, 0]  
+    X = data[:, feature_set]
+    correct = 0
 
+    for i in range(N):
+        best_distance = float('inf')
+        best_label = None
+        for j in range(N):
+            if i == j:
+                continue
+            distance = euclidean_distance(X[i], X[j])
+            if distance < best_distance:
+                best_distance = distance
+                best_label = labels[j]
+        if best_label == labels[i]:
+            correct += 1
+    return correct / N
 
+#check if data is loaded correctly
+if __name__ == "__main__":
+    #data = load_data('CS170_Large_DataSet__49.txt')
+    data = load_data('CS170_Small_DataSet__94.txt')
+    #test the nearest neighbor classifier
+    all_features = list(range(1, data.shape[1]))
+    print(nearest_neighbor(data, all_features))
