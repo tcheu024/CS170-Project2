f852b98cb47ac521c3df5f756937317b8805a1a7
Tim C <timc@Tims-MacBook-Pro.local>
2026-03-14 21:32:50 -0700

    added simple UI and fixed some bugs

--- main.py
+++ main.py
@@ -97,9 +97,47 @@ def backward_elimination(data_):
 #check if data is loaded correctly
 if __name__ == "__main__":
     #data = load_data('CS170_Large_DataSet__49.txt')
-    data = load_data('CS170_Small_DataSet__94.txt')
+    #data = load_data('CS170_Small_DataSet__94.txt')
     #test the nearest neighbor classifier
     #all_features = list(range(1, data.shape[1]))
     #print(nearest_neighbor(data, all_features))
     ##forward_selection(data)
-    backward_elimination(data)
+    #backward_elimination(data)
+
+    filename = input("Enter the 1 for big dataset or 2 for small dataset: ")
+    if filename == "1":
+        data = load_data('CS170_Large_DataSet__49.txt')
+    elif filename == "2":
+        data = load_data('CS170_Small_DataSet__94.txt')
+    else:
+        print("Invalid input, defaulting to small dataset for faster testing.")
+        data = load_data('CS170_Small_DataSet__94.txt')
+    
+    num_features = data.shape[1] - 1
+    num_features = data.shape[0]
+    print(f"Number of features: {num_features}")
+
+    all_features = list(range(1, data.shape[1]))
+    accuracy_all = nearest_neighbor(data, all_features)
+    print(f"Accuracy using all features: {accuracy_all:.4f}")
+
+    print("Type 1 for forward selection or 2 for backward elimination: ")
+    selection = input("Enter your choice: ")
+
+    if selection == "1":
+        forward_selection(data)
+    elif selection == "2":
+        backward_elimination(data)
+    else:
+        print("Invalid selection.")
+
+
+
+#forward selection on small dataset:
+#Feature set [11, 14, 4, 15, 3, 12, 16, 13, 2, 6, 5, 7, 10, 9, 1, 8] was best, accuracy is 0.7120
+#backward elimination on small dataset:
+#Feature set [11] was best, accuracy is 0.8120
+
+
+#success
+
